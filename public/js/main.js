0718047f89dffe190428ca577d7516ca193611df
LuiiVasquez <luivasque95@gmail.com>
2022-07-16 00:49:50 -0600

    Adicion de spots de informacion y cambio de imagenes 360

--- public/js/main.js
+++ public/js/main.js
@@ -3,49 +3,71 @@
 window.addEventListener('load', () => {
 
     // imagen a mostrar
-    const panoramaImage = new PANOLENS.ImagePanorama( '../img/uvga.jpg' );
-    const panoramaImage2 = new PANOLENS.ImagePanorama( '../img/uvga2.jpg' );
+    const panoramaImage = new PANOLENS.ImagePanorama( '../img/maker1.jpg' );
+    const panoramaImage2 = new PANOLENS.ImagePanorama( '../img/maker2.jpg' );
     // contenedor de la imagen
     const imageContainer = document.querySelector('.image-container');
     // contenedor de panel de informacion
-    const panelContainer = document.querySelector('.panel');
+    const panelMadera = document.querySelector('#madera');
+    const panelCepilladora = document.querySelector('#cepilladora');
+    const panelSierra = document.querySelector('#sierra');
+    const panelHerramienta3= document.querySelector('#herra3');
 
-    // viewer
+    // viewer contenedor
     const viewer = new PANOLENS.Viewer({
         container: imageContainer,
-        autoRotate: true,
-        autoRotateSpeed: 0.3
+        // autoRotate: true,
+        autoRotateSpeed: 0.3,
         // remover los controles
         //controlBar: false
+        cameraFov: 80
     });
 
-    // lugar 1
-    var infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
-    infospot.position.set( 0, -2000, -5000 );
-    infospot.addHoverText( "A la cafeteria" );
-    //infospot.addHoverElement( panelContainer, 150 );
-    infospot.addEventListener( 'click', function(){
-        viewer.setPanorama( panoramaImage2 );
-      } );
+    // funcion para ir a otro panorama (metodo 1)
+    function botonNuevaView(x,y,z, hoverText, nuevoPanorama){
+        let panorama = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
+        panorama.position.set(x, y, z);
+        panorama.addHoverText(hoverText);
+        panorama.addEventListener( 'click', function(){
+            viewer.setPanorama(nuevoPanorama);
+          } );
 
-      // lugar 2
-    var infospot2 = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
-    infospot2.position.set( 0, -2000, -5000 );
-    infospot2.addHoverText( "Al centro" );
-    //infospot.addHoverElement( panelContainer, 150 );
-    infospot2.addEventListener( 'click', function(){
-        viewer.setPanorama( panoramaImage );
-    } );
+          return panorama;
+    }
 
-    // panel de informacion 
-    var infospotinfo = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
-    infospotinfo.position.set( 0, 1000, -3000 );
-    //infospotinfo.addHoverText( "Informacion" );
-    infospotinfo.addHoverElement( panelContainer, 150 );
+    // funcion para crear infospots (mostrar panel)
+    function infoSpotContenido(x, y, z, contenido){
+        let infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
+        infospot.position.set(x, y, z);
+        infospot.addHoverElement( contenido, 150 );
 
+        return infospot;
+    }
 
-    panoramaImage.add(infospot, infospotinfo);
-    panoramaImage2.add(infospot2);
+    // funcion para crear infospots (mostrar texto)
+    function infoSpotTexto(x, y, z, texto){
+        let infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
+        infospot.position.set(x, y, z);
+        infospot.addHoverText(texto);
+
+        return infospot;
+    }
+
+    // funcion para ir a otro panorama
+    function linkPanorama(actualPanorama, nuevoPanorama, x, y, z, escala = 500){
+        actualPanorama.link( nuevoPanorama, new THREE.Vector3(x, y, z), escala);
+    }
+
+    const axesHelper = new THREE.AxesHelper( 5 );
+    viewer.add( axesHelper );
+
+    panoramaImage.add(infoSpotContenido(-7000, 1300, -1500, panelMadera));
+    panoramaImage.add(infoSpotContenido(-7000, -1500, 700, panelCepilladora));
+    panoramaImage.add(infoSpotContenido(-7000, -1200, -2200, panelSierra));
+    panoramaImage.add(infoSpotContenido(-7000, -900, -5200, panelHerramienta3));
+    panoramaImage.add(infoSpotTexto(-500, 0, -5200, "SALIDA", 25));
+    linkPanorama(panoramaImage, panoramaImage2, 300, -2000, 8000);
+    linkPanorama(panoramaImage2, panoramaImage, -1000, -900, -5000, 400);
 
     // na;adir la imagen al viewer
     viewer.add(panoramaImage, panoramaImage2);
